5bde17306ef6a0e3bbd16e4100e6d09a92333987
cleversystech-dev <3901-nayahyao@users.noreply.gitlab.int.nbyt.fr>
2022-07-20 08:40:21 +0200

    finalize template

--- App.tsx
+++ App.tsx
@@ -1,21 +1,26 @@
 import * as React from 'react';
-import {Button, Text, View} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import { testcoMPOSANT } from './src/app/components/stack';
+import { Button, Platform, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import ReactNativeHapticFeedback from 'react-native-haptic-feedback';
+ import { testcoMPOSANT } from './src/app/components/stack';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
 function DetailsScreen() {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Details!</Text>
     </View>
   );
 }
+const MyScreen = () => {
+  ReactNativeHapticFeedback.trigger("impactLight");
+  }
 
-function HomeScreen({navigation}) {
+function HomeScreen({ navigation }) {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Home screen</Text>
       <Button
         title="Go to Details"
@@ -24,15 +29,31 @@ function HomeScreen({navigation}) {
     </View>
   );
 }
+const hapticOptions = {
+  enableVibrateFallback: false,
+  ignoreAndroidSystemSettings: true,
+};
 
-function SettingsScreen({navigation}) {
+const hapticTriggerType = Platform.select({
+  ios: 'notificationSuccess',
+  android: 'impactMedium'
+});
+
+function SettingsScreen({ navigation }) {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Settings screen</Text>
-      <Button
+      <Button title="Learn More4"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+        onPress={() => {
+console.log('test 123');
+ReactNativeHapticFeedback.trigger(hapticTriggerType, hapticOptions)
+        }} />
+      {/* <Button
         title="Go to Details"
         onPress={() => navigation.navigate('Details')}
-      />
+      /> */}
     </View>
   );
 }
@@ -42,7 +63,7 @@ const HomeStack = createNativeStackNavigator();
 function HomeStackScreen() {
   return (
     <HomeStack.Navigator>
-      <HomeStack.Screen name="Home" component={testcoMPOSANT} />
+
       <HomeStack.Screen name="Details" component={DetailsScreen} />
     </HomeStack.Navigator>
   );
@@ -61,11 +82,34 @@ function SettingsStackScreen() {
 
 const Tab = createBottomTabNavigator();
 
+
+
+
+
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator screenOptions={{headerShown: false}}>
-        <Tab.Screen name="Home" component={HomeStackScreen} />
+      <Tab.Navigator screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'ios-information-circle'
+                : 'ios-information-circle-outline';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'ios-list-box' : 'ios-list';
+            }
+return <Icon name="rocket" size={30} color="#900" />
+            // You can return any component that you like here!
+            //return <Icon name="facebook" size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+        }}>
+        <Tab.Screen name="Home" component={testcoMPOSANT} />
         <Tab.Screen name="Settings" component={SettingsStackScreen} />
       </Tab.Navigator>
     </NavigationContainer>
--- src/app/components/stack.tsx
+++ src/app/components/stack.tsx
@@ -1,312 +1,157 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import {
-    Flex,
-    Divider,
-    Text,
-    ScrollView,
-    VStack,
-    Center,
-    NativeBaseProvider,
-    Icon,
-    Switch,
-    IconButton,
-    Box,
-    View,
-    Heading,
-    Button,
+  Flex,
+  CheckIcon,
+  Divider,
+  Text,
+  ScrollView,
+  VStack,
+  Center,
+  NativeBaseProvider,
+  Switch,
+  IconButton,
+  Box,
+  View,
+  Heading,
+  Button,
+  Progress,
 } from 'native-base';
-import { faRedo,faArrowAltCircleDown } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
-import { Platform,Vibration } from 'react-native';
 import ReactNativeHapticFeedback from 'react-native-haptic-feedback';
-import {sendMessage} from '../../module/common/websocket';
 import IO from 'socket.io-client';
-
-const fontAwesome = {
-    iconFamily: 'FontAwesome',
-    iconFontSize: (Platform.OS === 'ios') ? 30 : 28,
-    iconMargin: 7,
-    iconLineHeight: (Platform.OS === 'ios') ? 37 : 30,
-}
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
 const options = {
-    enableVibrateFallback: false,
-    ignoreAndroidSystemSettings: true
-  };
-  
-  ReactNativeHapticFeedback.trigger("impactLight", options);
-
+  enableVibrateFallback: false,
+  ignoreAndroidSystemSettings: true,
+};
 
 export function testcoMPOSANT() {
-    return (
-        <NativeBaseProvider >
-            <Center flex={1} px="3">
-                <ScrollView
-                    alignSelf="stretch"
-                    h="80"
-                    _contentContainerStyle={{
-                        px: '20px',
-                        mb: '4',
-                        minW: '72',
-                    }}>
-                    <VStack space={2} alignItems="center" alignSelf="stretch">
-                        <Center
-                            alignItems="flex-end"
-                            alignSelf="stretch"
-                            h="20"
-                            bg="indigo.300"
-                            rounded="md"
-                            shadow={5}>
-                            <Icon as={FontAwesomeIcon} icon={faRedo} />
-                        </Center>
-                        <Center
-                            alignSelf="stretch"
-                            h="20"
-                            bg="indigo.500"
-                            rounded="md"
-                            shadow={5}>
-                        </Center>
+  return (
+    <NativeBaseProvider>
+      <Center flex={1}>
+        <ScrollView alignSelf="stretch" _contentContainerStyle={{px: '3'}}>
+          <VStack space={3} alignItems="center" alignSelf="stretch">
+            <Center
+              alignSelf="stretch"
+              h="20"
+              bg="indigo.500"
+              rounded="md"
+              shadow={5}
+            />
+            <Box alignSelf="stretch" bg="#ededed" rounded="md" shadow={5}>
+              <View style={{flexDirection: 'row'}} mt="3" mb="3">
+                <Box style={{flex: 1, alignItems: 'flex-start'}} px="1">
+                  <Flex direction="row">
+                    <IconButton
+                      size={21}
+                      onPress={() => {
+                        ReactNativeHapticFeedback.trigger(
+                          'impactMedium',
+                          options,
+                        );
+                        console.log('open execution settings');
+                      }}
+                      icon={<Icon name="bolt" size={21} color="#0a90b2" />}
+                    />
+                  </Flex>
+                </Box>
 
-                        <Center
-                            alignSelf="stretch"
-                            h="20"
-                            bg="indigo.300"
-                            rounded="md"
-                            shadow={5}>
-                            <View style={{ flexDirection: 'row' }}>
-                                <Box>
-                                    <Flex direction="row" h="62" p="4">
-                                        <IconButton icon={<Icon as={FontAwesomeIcon} icon={faArrowAltCircleDown} />} borderRadius="full" _icon={{
-                                            color: 'coolGray.50',
-                                            size: 'md'
-                                        }} _hover={{
-                                            bg: 'coolGray.800:alpha.20'
-                                        }} _pressed={{
-                                            bg: 'coolGray.800:alpha.20',
-                                            _icon: {
-                                                name: 'emoji-flirt'
-                                            },
-                                            _ios: {
-                                                _icon: {
-                                                    size: 'md'
-                                                }
-                                            }
-                                        }} _ios={{
-                                            _icon: {
-                                                size: 'md'
-                                            }
-                                        }} />
-                                        <Divider
-                                            bg="emerald.500"
-                                            thickness="2"
-                                            mx="-1"
-                                            orientation="vertical"
-                                        />
-                                        <IconButton
-                                            onPress={() => {
-                                                ReactNativeHapticFeedback.trigger("impactLight", options);
-                                               //Vibration.vibrate();
-                                                console.log('ça marche');
-                                            }}
+                <Box style={{flex: 5, alignItems: 'center'}}>
+                  <Heading
+                    size="sm"
+                    alignSelf="center"
+                    color="grey"
+                    numberOfLines={1}
+                    ellipsizeMode="tail">
+                    Irrigitation ferme
+                    <Switch defaultIsChecked color="#0a90b2" size={'sm'} />
+                  </Heading>
+                </Box>
 
-                                            icon={<Icon as={FontAwesomeIcon} icon={faRedo} />} borderRadius="full" _icon={{
-                                                color: 'coolGray.50',
-                                                size: 'md'
-                                            }} _hover={{
-                                                bg: 'coolGray.800:alpha.20'
-                                            }} _pressed={{
-                                                bg: 'coolGray.800:alpha.20',
-                                                _icon: {
-                                                    name: 'emoji-flirt'
-                                                },
-                                                _ios: {
-                                                    _icon: {
-                                                        size: 'md'
-                                                    }
-                                                }
-                                            }} _ios={{
-                                                _icon: {
-                                                    size: 'md'
-                                                }
-                                            }} />
-                                        <Divider
-                                            bg="indigo.100"
-                                            thickness="2"
-                                            mx="-1"
-                                            orientation="vertical"
-                                        />
-                                    </Flex>
-                                </Box>
+                <Box style={{alignItems: 'flex-end'}} px="1">
+                  <IconButton
+                    size={22}
+                    onPress={() => {
+                      ReactNativeHapticFeedback.trigger(
+                        'impactMedium',
+                        options,
+                      );
+                      console.log('open settings');
+                    }}
+                    icon={<Icon name="cog" size={22} color="#0a90b2" />}
+                  />
+                </Box>
+              </View>
 
-                                <Box style={{ flex: 2, alignItems: 'center' }}>
-                                    <Flex direction="row" h="62" p="3" style={{ textAlign: 'flex-end' }}>
-                                        <Heading size="sm" alignSelf="center">
-                                            Irrigitation
-                                        </Heading>
-                                    </Flex>
-                                </Box>
+              <Center alignSelf="stretch" mb="3" >
+                {/* display={'none'} */}
+                <View style={{flexDirection: 'row'}}>
+                  <Heading
+                    size="xs"
+                    alignSelf="center"
+                    color="grey"
+                    numberOfLines={1}
+                    ellipsizeMode="tail"
+                    ml="2">
+                    01:12:45
+                  </Heading>
+                  <Box style={{flex: 9, alignItems: 'center'}} mt={1}>
+                    <Flex direction="row" px="2">
+                      <Box style={{flex: 5}}>
+                        <Progress
+                          size="md"
+                          rounded="md"
+                          value={10}
+                          mx="1"
+                          _filledTrack={{bg: '#0a90b2'}}
+                        />
+                      </Box>
+                    </Flex>
+                  </Box>
 
-                                <Box style={{ flex: 1, alignItems: 'flex-end' }}>
-                                    <Flex direction="row" h="62" p="4">
-                                        <Switch defaultIsChecked colorScheme="emerald" size="md" />
-                                    </Flex>
-                                </Box>
-                            </View>
-                        </Center>
-                        <Center
-                            alignSelf="stretch"
-                            h="20"
-                            bg="indigo.500"
-                            rounded="md"
-                            shadow={5}
-                        >
-                            <Box alignItems="center">
-      <Button onPress={() => {
-        const socket = IO('http://192.168.1.15:5001', {
-            forceNew: true,
-        });
-        socket.on('connection', () => console.log('Connection'));
-        ReactNativeHapticFeedback.trigger("impactLight", options);
-      }}>Click Me</Button>
-    </Box>
-                            </Center>
-                        <Center
-                            alignSelf="stretch"
-                            h="20"
-                            bg="indigo.700"
-                            rounded="md"
-                            shadow={5}
-                        />
-                        <Center
-                            alignSelf="stretch"
-                            h="20"
-                            bg="indigo.300"
-                            rounded="md"
-                            shadow={5}
-                        />
-                        <Center
-                            alignSelf="stretch"
-                            h="20"
-                            bg="indigo.500"
-                            rounded="md"
-                            shadow={5}
-                        />
-                        <Center
-                            alignSelf="stretch"
-                            h="20"
-                            bg="indigo.700"
-                            rounded="md"
-                            shadow={5}
-                        />
-                        <Center
-                            alignSelf="stretch"
-                            h="20"
-                            bg="indigo.300"
-                            rounded="md"
-                            shadow={5}
-                        />
-                        <Center
-                            alignSelf="stretch"
-                            h="20"
-                            bg="indigo.500"
-                            rounded="md"
-                            shadow={5}
-                        />
-                        <Center
-                            alignSelf="stretch"
-                            h="20"
-                            bg="indigo.700"
-                            rounded="md"
-                            shadow={5}
-                        />
-                        <Center
-                            alignSelf="stretch"
-                            h="20"
-                            bg="indigo.300"
-                            rounded="md"
-                            shadow={5}
-                        />
-                        <Center
-                            alignSelf="stretch"
-                            h="20"
-                            bg="indigo.500"
-                            rounded="md"
-                            shadow={5}
-                        />
-                        <Center
-                            alignSelf="stretch"
-                            h="20"
-                            bg="indigo.700"
-                            rounded="md"
-                            shadow={5}
-                        />
-                        <Center
-                            alignSelf="stretch"
-                            h="20"
-                            bg="indigo.300"
-                            rounded="md"
-                            shadow={5}
-                        />
-                        <Center
-                            alignSelf="stretch"
-                            h="20"
-                            bg="indigo.500"
-                            rounded="md"
-                            shadow={5}
-                        />
-                        <Center
-                            alignSelf="stretch"
-                            h="20"
-                            bg="indigo.700"
-                            rounded="md"
-                            shadow={5}
-                        />
-                        <Center
-                            alignSelf="stretch"
-                            h="20"
-                            bg="indigo.300"
-                            rounded="md"
-                            shadow={5}
-                        />
-                        <Center
-                            alignSelf="stretch"
-                            h="20"
-                            bg="indigo.500"
-                            rounded="md"
-                            shadow={5}
-                        />
-                        <Center
-                            alignSelf="stretch"
-                            h="20"
-                            bg="indigo.700"
-                            rounded="md"
-                            shadow={5}
-                        />
-                        <Center
-                            alignSelf="stretch"
-                            h="20"
-                            bg="indigo.300"
-                            rounded="md"
-                            shadow={5}
-                        />
-                        <Center
-                            alignSelf="stretch"
-                            h="20"
-                            bg="indigo.500"
-                            rounded="md"
-                            shadow={5}
-                        />
-                        <Center
-                            alignSelf="stretch"
-                            h="20"
-                            bg="indigo.700"
-                            rounded="md"
-                            shadow={5}
-                        />
-                    </VStack>
-                </ScrollView>
+                  <Box style={{flex: 1, alignItems: 'flex-end'}}>
+                    <Flex direction="row" px="1">
+                      <IconButton
+                        size={21}
+                        onPress={() => {
+                          ReactNativeHapticFeedback.trigger(
+                            'impactMedium',
+                            options,
+                          );
+                          console.log('get information');
+                        }}
+                        icon={
+                          <Icon name="info-circle" size={21} color="#0a90b2" />
+                        }
+                      />
+                    </Flex>
+                  </Box>
+                </View>
+              </Center>
+            </Box>
+            <Center
+              alignSelf="stretch"
+              h="20"
+              bg="indigo.500"
+              rounded="md"
+              shadow={5}>
+              <Box alignItems="center">
+                <Button
+                  onPress={() => {
+                    const socket = IO('http://192.168.1.15:5001', {
+                      forceNew: true,
+                    });
+                    socket.on('connection', () => console.log('Connection'));
+                    ReactNativeHapticFeedback.trigger('impactLight', options);
+                  }}>
+                  Click Me
+                </Button>
+              </Box>
             </Center>
-        </NativeBaseProvider>
-    );
+          </VStack>
+        </ScrollView>
+      </Center>
+    </NativeBaseProvider>
+  );
 }
-
